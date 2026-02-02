20225251c552b07846f63a8924bf9a7be875b88c
vickykumar3510 <vicky.kumar3510@gmail.com>
2026-02-03 02:25:24 +0530

    chore: backend updated

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
     credentials: true
   },
-  transports: ["polling"],
+  transports: ["websocket", "polling"],
 });
 
 app.use(cors({
